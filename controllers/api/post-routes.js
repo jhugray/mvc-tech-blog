bf92e38195f5491e131cb4bca27d809f9ba72902
Jess Hause Ugray <jess.ugray@gmail.com>
2021-11-22 13:33:59 -0500

    edit/delete post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Post, User, Comment} = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // get all users
 router.get('/', (req, res) => {
@@ -73,26 +74,26 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
-  Post.create({
-    title: req.body.title,
-    body: req.body.body,
-    user_id: req.session.user_id
-  })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+router.post('/', withAuth, (req, res) => {
+  if (req.session) {
+    Post.create({
+        title: req.body.title,
+        body: req.body.body,
+        user_id: req.session.user_id
+      })
+        .then(dbPostData => res.json(dbPostData))
+            .catch(err => {
+              console.log(err);
+              res.status(500).json(err);
+            });
+  }  
 });
 
 
 router.put('/:id', (req, res) => {
   Post.update(
     {
-      title: req.body.title
-    },
-    {
+      title: req.body.title,
       body: req.body.body
     },
     {
