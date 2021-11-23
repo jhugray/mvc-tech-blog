944b448659d8060a0ff22e1f234bfe390dd8400d
Jess Hause Ugray <jess.ugray@gmail.com>
2021-11-23 14:33:22 -0500

    email removed

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -69,14 +69,14 @@ router.post('/', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+ 
   User.findOne({
     where: {
-      email: req.body.email
+      username: req.body.username
     }
   }).then(dbUserData => {
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
+      res.status(400).json({ message: 'No user exists with that username!' });
       return;
     }
 
@@ -109,7 +109,7 @@ router.post('/logout', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
