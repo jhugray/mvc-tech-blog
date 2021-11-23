e266266749521eb5f1d75f019f865e36a7958679
Jess Hause Ugray <jess.ugray@gmail.com>
2021-11-23 15:41:16 -0500

    refactored, welcome user message added to dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -34,52 +34,10 @@ router.get('/', withAuth, (req, res) => {
     .then(dbPostData => {
       // serialize data before passing to template
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('dashboard', { posts, loggedIn: true });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-
-router.get('/', withAuth, (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: [
-      'id',
-      'body',
-      'title',
-      'created_at'
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
-
-      const post = dbPostData.get({ plain: true });
-
-      res.render('edit-post', {
-        post,
-        loggedIn: req.session.loggedIn
+      res.render('dashboard', { 
+        posts, 
+        username: req.session.username, 
+        loggedIn: true 
       });
     })
     .catch(err => {
@@ -87,6 +45,8 @@ router.get('/', withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
+
+
 
 router.get('/edit/:id', withAuth, (req, res) => {
   Post.findByPk(req.params.id, {
