126a65effd899ceebfea7edb9e772fbc3290ac72
Jess Hause Ugray <jess.ugray@gmail.com>
2021-11-23 11:42:42 -0500

    email removed

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -4,17 +4,14 @@ const { User, Post } = require('../models');
 const userdata = [
   {
     username: 'user1',
-    email: 'user1@email.com',
     password: 'badpassword1'
   },
   {
     username: 'user2',
-    email: 'user2@email.com',
     password: 'badpassword2'
   },
   {
     username: 'user3',
-    email: 'user3@email.com',
     password: 'badpassword3'
   }
 ];
