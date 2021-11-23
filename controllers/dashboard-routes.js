7f8b6835eb0a9d2d18f97ee46cbc09370462ddb9
Jess Hause Ugray <jess.ugray@gmail.com>
2021-11-23 06:09:59 -0500

    css colour and links changed

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -128,4 +128,39 @@ router.get('/edit/:id', withAuth, (req, res) => {
     });
 });
 
+
+router.get('/:id', (req, res) => {
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'body', 'created_at']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
+      }
+    ]
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
